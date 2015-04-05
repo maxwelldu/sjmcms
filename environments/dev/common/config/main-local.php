95c470d7ff36628d055bea918de99e56ef92fc7b
maxwelldu <dcj3sjt@163.com>
2015-04-05 22:10:45 +0800

    修改了环境变量中的数据库连接信息

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=maxwelldu.mysql.rds.aliyuncs.com;dbname=sjmcms',
+            'username' => 'maxwelldu',
+            'password' => 'yu13jiu14',
             'charset' => 'utf8',
         ],
         'mailer' => [
